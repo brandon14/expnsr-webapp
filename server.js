3b1cdc1cd46cea170986f6218e22f24db46a1774
Brandon Clothier <brandon14125@gmail.com>
2017-01-29 18:46:54 -0500

    Move webpack middle ware out to devDeps
    Configure a function to get the date of the last modified file and set up a route in express to return last modified file time in JSON.
    
    Fix webpack.config.js serve path for local deployment  using my apache2 proxy to proxy everything to the nodejs app.

--- server.js
+++ server.js
@@ -5,15 +5,19 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+const _ = require('underscore');
+const fs = require('fs');
 const config = require('./webpack.config.js');
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 5000 : process.env.PORT;
+const isDev = process.env.NODE_ENV !== 'production';
+const port = isDev ? 5000 : process.env.PORT;
 const app = express();
 
-if (isDeveloping) {
+// If this is a development environment, load the webpack middlewares to serve webpack bundles
+if (isDev) {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
@@ -30,18 +34,52 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
+
+  app.get('/last-modified', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "last-modified": getMostRecentFile('src') }));
+  });
+
   app.get('*', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
   });
-} else {
+} else { // End if (isDev)
+  // In production environments, set the express app to use the dist/ directory where the webpack bundles and assets are located at
   app.use(express.static(__dirname + '/dist'));
+
+  app.get('/last-modified', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "last-modified": getMostRecentFile('src') }));
+  });
+
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
-}
+} // End if (isDev) {} else {}
 
+// Listen for requests using the express server
 app.listen(port, function onStart(err) {
   if (err) { console.log(err); }
+
   console.info('==> Listening on port %s.', port);
 });
+
+const walkSync = function(dir, filelist) {
+  filelist = filelist || [];
+
+  fs.readdirSync(dir).forEach(function(file) {
+    filelist = fs.statSync(path.join(dir, file)).isDirectory() ? walkSync(path.join(dir, file), filelist) : filelist.concat(path.join(dir, file));
+  });
+  return filelist;
+}
+
+const getMostRecentFile = function(dir) {
+  var files = walkSync(dir);
+
+  var max = _.max(files, function (file) {
+    return fs.statSync(file).mtime;
+  });
+
+  return fs.statSync(max).mtime.toLocaleString('en-US');
+}
